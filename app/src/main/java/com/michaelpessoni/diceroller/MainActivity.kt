7d4d7e240fb0867872a77d0a171dae588accb4f2
michael-pessoni <pessoni94@gmail.com>
2021-10-26 17:25:10 -0300

    Adding Dice Images

--- app/src/main/java/com/michaelpessoni/diceroller/MainActivity.kt
+++ app/src/main/java/com/michaelpessoni/diceroller/MainActivity.kt
@@ -18,7 +18,16 @@ class MainActivity : AppCompatActivity() {
 
         binding.rollButton.setOnClickListener {
             val randoInt = Random().nextInt(6) + 1
-            binding.resultText.text = randoInt.toString()
+            binding.diceImage.setImageResource(
+                when(randoInt) {
+                    1 -> R.drawable.dice_1
+                    2 -> R.drawable.dice_2
+                    3 -> R.drawable.dice_3
+                    4 -> R.drawable.dice_4
+                    5 -> R.drawable.dice_5
+                    else -> R.drawable.dice_6
+                }
+            )
         }
     }
 }
