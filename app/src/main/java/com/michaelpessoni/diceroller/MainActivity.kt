b7bcd43abf8f4de60cb72a06cb43d75a20da11e2
michael-pessoni <pessoni94@gmail.com>
2021-10-26 17:36:36 -0300

    Refactor

--- app/src/main/java/com/michaelpessoni/diceroller/MainActivity.kt
+++ app/src/main/java/com/michaelpessoni/diceroller/MainActivity.kt
@@ -2,7 +2,6 @@ package com.michaelpessoni.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.michaelpessoni.diceroller.databinding.ActivityMainBinding
 import java.util.*
 
@@ -17,17 +16,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.rollButton.setOnClickListener {
-            val randoInt = Random().nextInt(6) + 1
-            binding.diceImage.setImageResource(
-                when(randoInt) {
-                    1 -> R.drawable.dice_1
-                    2 -> R.drawable.dice_2
-                    3 -> R.drawable.dice_3
-                    4 -> R.drawable.dice_4
-                    5 -> R.drawable.dice_5
-                    else -> R.drawable.dice_6
-                }
-            )
+            val randomInt = Random().nextInt(6) + 1
+            val imageAsset =  when(randomInt) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            binding.diceImage.setImageResource(imageAsset)
         }
     }
 }
